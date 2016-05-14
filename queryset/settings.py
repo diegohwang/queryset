67665d44f8077d152497cee54d96766cd7c9c73f
diego_home <diegohwang@163.com>
2016-05-14 19:31:51 +0800

    change app

--- queryset/settings.py
+++ queryset/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'cellphone',
+    
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,10 +78,21 @@ WSGI_APPLICATION = 'queryset.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'queryset',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST':'127.0.0.1',
+        'PORT': '3306',
     }
+    
 }
 
 
