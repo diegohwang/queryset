d0a51fd16c95889f574e44b4eefd51b5bac150c8
diegohwang <diegohwang@163.com>
2016-05-16 18:30:25 +0800

    update create demo

--- create_demo_record.py
+++ create_demo_record.py
@@ -37,11 +37,49 @@ class QuerySetOperation():
         for s in all_site:
             print 'the site_id %d is %s' % (s.site_id, s.site_name)
     
+    #2.2 切片查询(不支持负索引)
     def get_slice(self):
         all_site = site.objects.all()
         slice_site = all_site[0:2]
         for s in slice_site:
             print s
+    
+    #2.3 条件唯一查询
+    def get_one(self):
+        #site_name为'优酷'的含义多条时，会报错
+        st = site.objects.get(site_name='优酷')
+        print st
+        
+    #2.4 条件过滤
+    #2.4.1 等于
+    def filter_equal(self):
+        st = site.objects.filter(site_code='youku')
+        #st = site.objects.filter(site_code_exact='youku')
+        print st
+    #2.4.2 忽略大小写的等于
+    def filter_equal_iexact(self):
+        st = site.objects.filter(site_code__ieact='YouKu')
+        print st
+    #2.4.3 包含
+    def filter_contains(self):
+        st = site.objects.filter(site_code__contains='ku')
+        print st
+    #2.4.4 忽略大小写的包含
+    def filter_icontain(self):
+        st = site.objects.filter(site_code__contains='KU')
+        print st
+    #2.4.5 正则表达式过滤
+    def filter_regex(self):
+        st = site.objects.filter(site_code__regex='^you')
+        print st
+    #2.4.6 忽略正则表达式过滤
+    def filter_iregex(self):
+        st = site.objects.filter(site_code__iregex='^YoU')
+        print st
+    #2.4.7 排除
+    def exclude(self):
+        st = site.objects.exclude(site_code__exact='youku')
+        print st
         
 if __name__ == "__main__":
     qso = QuerySetOperation()
