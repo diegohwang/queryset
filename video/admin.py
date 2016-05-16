bb44c960f550f0baebda8da9d9b58e9fd096e971
diegohwang <diegohwang@163.com>
2016-05-16 15:01:24 +0800

    register admin

--- video/admin.py
+++ video/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from models import site, media
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display=('title', 'url')
+    
+class SiteAdmin(admin.ModelAdmin):
+    list_dislplay=('site_name')
+    
+admin.site.register(site,SiteAdmin)
+admin.site.register(media,MediaAdmin)
